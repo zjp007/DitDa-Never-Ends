bc027c1be150bb93d1f71a457cbb3862d6bfc737
Zhao_Acg_PC <906030149zhao@gmail.com>
2026-09-05 17:31:20 +0800

    first commit

---
+++ Assets/Scripts/LogoAndStartMove.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PrimeTween;
+
+public class LogoAndStartMove : MonoBehaviour
+{
+    [Header("初始坐标")]
+    public Vector2 initPos;
+    [Header("结束坐标")]
+    public Vector2 endPos;
+    [Header("动画时长")]
+    public float animationDuration;
+    
+    void Start()
+    {
+        Sequence.Create()
+            .ChainCallback(target: this, tarhet =>
+            {
+                transform.localPosition = initPos;
+            })
+            .Chain(Tween.LocalPosition(transform, endPos, animationDuration, ease:Ease.InOutSine));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
